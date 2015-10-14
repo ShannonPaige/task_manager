105de6e08e9d4effa5b87908ef9095ef3f202d7a
ShannonPaige <spaige@gmail.com>
2015-10-14 16:22:17 -0600

    feature test tweak

--- test/features/user_can_delete_a_task_test.rb
+++ test/features/user_can_delete_a_task_test.rb
@@ -3,10 +3,12 @@ require_relative "../test_helper"
 class DeleteTaskTest < FeatureTest
 
   def test_user_can_delete_a_task
-    create_tasks(2)
+    create_tasks(3)
     visit "/tasks"
 
     assert_equal "/tasks" , current_path
+    assert page.has_content?("this is 2 title")
+    assert page.has_css?("#id2")
 
     within ("#form-id2") do
       click_button "Delete"
